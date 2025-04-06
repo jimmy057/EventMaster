f4bf42ff848649afa8d4e3da690941cf0e5b79d6
jimmy057 <jimmypolanco2540@gmail.com>
2025-04-06 10:45:33 -0400

    Se creo el service del detalle de los eventos

---
+++ Services/EventosDetalleService.cs
@@ -0,0 +1,21 @@
+﻿using EventMaster.Data;
+using EventMaster.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MegatoursRD.Services;
+
+public class EventosDetalleService(IDbContextFactory<ApplicationDbContext> DbFactory)
+{
+	public async Task<List<EventosDetalle>> Listar(Expression<Func<EventosDetalle, bool>> criterio)
+	{
+		await using var contexto = await DbFactory.CreateDbContextAsync();
+		return await contexto.EventosDetalle
+			.Include(x => x.Evento)
+			.Include(x => x.Actividad)
+			.AsNoTracking()
+			.Where(criterio)
+			.ToListAsync();
+	}
+}
+
