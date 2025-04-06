37eb2c9f68037103ec3723ab43bb35206e924334
jimmy057 <jimmypolanco2540@gmail.com>
2025-04-06 18:09:39 -0400

    #

--- obj/Debug/net9.0/EventMaster.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventMaster.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventMaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e8ea0c1b3ed30f5eeae22ec71758341ecebf8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventMaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventMaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
