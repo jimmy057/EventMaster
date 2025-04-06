7c9ac59d89c03ff8066b49f51312f8a89be8917d
jimmy057 <jimmypolanco2540@gmail.com>
2025-04-06 10:43:56 -0400

    Se agregaron los dbset para crear las tablas en la DB

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
+using EventMaster.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace EventMaster.Data
+namespace EventMaster.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+	: IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
+	public DbSet<Clientes> Clientes { get; set; }
+	public DbSet<Administradores> Administradores { get; set; }
+	public DbSet<Eventos> Eventos { get; set; }
+	public DbSet<EventosDetalle> EventosDetalle { get; set; }
+	public DbSet<Actividades> Actividades { get; set; }
 }
