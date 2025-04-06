493b37ae4a943a3f51b02aa95cbc66e8637f561d
jimmy057 <jimmypolanco2540@gmail.com>
2025-04-06 10:44:42 -0400

    Se creo una clase filtro extra para no declarar variables en cada componente a la hora de listar

---
+++ Models/Filtro.cs
@@ -0,0 +1,10 @@
+﻿namespace EventMaster.Models;
+
+public class Filtro
+{
+	public string? CriterioBusqueda { get; set; }
+	public string? valorEnFiltro { get; set; }
+	public string? MensajeError { get; set; }
+	public DateTime? Desde { get; set; }
+	public DateTime? Hasta { get; set; }
+}
