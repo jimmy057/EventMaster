71b8cb11a4b12e3bb5d1a68cc6295718d6ca4aa8
jimmy057 <jimmypolanco2540@gmail.com>
2025-04-07 01:26:41 -0400

    Se creo el service para validar emails de usuarios

---
+++ Services/UsuariosService.cs
@@ -0,0 +1,17 @@
+﻿using EventMaster.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventMaster.Services;
+
+public class UsuariosService(IDbContextFactory<ApplicationDbContext> DbFactory)
+{
+	public async Task<bool> ValidarEmail(string email)
+	{
+		email.Trim().ToLower();
+		await using var contexto = await DbFactory.CreateDbContextAsync();
+		return await contexto.Users
+			.AnyAsync(x => x.Email.Trim().ToLower().Contains(email));
+	}
+}
+
+
