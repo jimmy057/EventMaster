6ab59df9ca67fa5309e59fd2a39481da7d0c4082
jimmy057 <jimmypolanco2540@gmail.com>
2025-04-07 04:00:53 -0400

    Se migraron los datos

---
+++ Data/Migrations/20250407054805_PasandoCampoIntAStringEnClaseAdministradores.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventMaster.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasandoCampoIntAStringEnClaseAdministradores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NombreUsuario",
+                table: "Administradores",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NombreUsuario",
+                table: "Administradores",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
