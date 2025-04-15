e1d02bd986b1c35b0d142ece255723d3a6fda032
Jimmy057 <jimmypolanco2540@gmail.com>
2025-04-14 22:28:00 -0400

    Se agregaron varios campos a la entidad

--- Models/Actividades.cs
+++ Models/Actividades.cs
@@ -6,11 +6,11 @@ public class Actividades
 {
 	[Key]
 	public int ActividadId { get; set; }
-	public string TituloActividad { get; set; }
-	public string TipoActividad { get; set; }
-	public string Descripcion { get; set; }
+	public string? TituloActividad { get; set; }
+	public string? TipoActividad { get; set; }
+	public string? Descripcion { get; set; }
 	public TimeOnly HoraInicio { get; set; }
 	public TimeOnly HoraFin { get; set; }
-	public int IdCliente { get; set; }
-	public int Cupos { get; set; } = 100;
+	public int? IdCliente { get; set; }
+	public int? Cupos { get; set; } = 100;
 }
